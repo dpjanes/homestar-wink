2beaca193980268922f0a632297fc4c22eea6ba5
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:27:36 -0500

    fixed up 'push' validation

--- WinkBridge.js
+++ WinkBridge.js
@@ -184,7 +184,7 @@ WinkBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",
